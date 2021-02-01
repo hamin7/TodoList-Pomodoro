3074f7fb31bb12c144d9b85c64ac19ff8fdad0e5
hamin_kt <ggamini7@gmail.com>
2021-02-01 22:03:11 +0900

    아이콘 추가 및 화이트 노이즈 바 디자인 변경

--- src/containers/Todos/Todos.js
+++ src/containers/Todos/Todos.js
@@ -11,7 +11,6 @@ import Loader from '../../components/UI/Loader/Loader';
 import Todo from './Todo/Todo';
 import Timer from '../../components/Timer';
 import * as actions from '../../store/actions';
-
 import WhiteNoise from '../../components/WhiteNoise/WhiteNoise';
 
 const Wrapper = styled.div`
@@ -92,9 +91,9 @@ const Todos = ({ todos, requested, userId }) => {
           </Button>
           <InputTodo opened={isAdding} close={() => setIsAdding(false)} />
           {content}
-          <WhiteNoise />
-        </InnerWrapper>
+        </InnerWrapper>       
       </Container>
+      <WhiteNoise />
     </Wrapper>
   );
 };
