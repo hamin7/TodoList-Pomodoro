395e2e443cfe200015c850ff4b35aae098a6235a
hamin_kt <ggamini7@gmail.com>
2021-02-02 10:32:04 +0900

    타이머 버튼 색상 #f75745 -> #f21283e

--- src/components/WhiteNoise/WhiteNoise.js
+++ src/components/WhiteNoise/WhiteNoise.js
@@ -16,23 +16,25 @@ import thunder from '../../audio/thunder.mp3';
 import winter_wave from '../../audio/winter_wave.mp3';
 import wood_with_axe from '../../audio/wood_with_axe.mp3';
 
+import babbling_stream_IMG from '../../assets/tomato.png';
+
 class App1 extends Component {
   constructor(props) {
     super(props);
     this.state = {
       audioCurrent: {},
       audioList: [
-        { title: '시냇물 소리', file: babbling_stream },
-        { title: '모닥불 소리', file: campfire },
-        { title: '열차 소리', file: commuter_train },
-        { title: '미용실 소리', file: haircut },
-        { title: '새 소리', file: morning_birds },
-        { title: '빗소리', file: rain },
-        { title: '해변 소리', file: summer_beach },
-        { title: '숲 소리', file: tent },
-        { title: '천둥 소리', file: thunder },
-        { title: '겨울 바람', file: winter_wave },
-        { title: '나무 베는 소리', file: wood_with_axe }
+        { title: '시냇물 소리', file: babbling_stream, img: babbling_stream_IMG },
+        { title: '모닥불 소리', file: campfire, img: babbling_stream_IMG },
+        { title: '열차 소리', file: commuter_train, img: babbling_stream_IMG },
+        { title: '미용실 소리', file: haircut, img: babbling_stream_IMG },
+        { title: '새 소리', file: morning_birds, img: babbling_stream_IMG },
+        { title: '빗소리', file: rain, img: babbling_stream_IMG },
+        { title: '해변 소리', file: summer_beach, img: babbling_stream_IMG },
+        { title: '숲 소리', file: tent, img: babbling_stream_IMG },
+        { title: '천둥 소리', file: thunder, img: babbling_stream_IMG },
+        { title: '겨울 바람', file: winter_wave, img: babbling_stream_IMG },
+        { title: '나무 베는 소리', file: wood_with_axe, img: babbling_stream_IMG }
       ]
     }
   }
